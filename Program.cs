5022a12a93a7ce760902ccf8acecbfb423671e6f
Cong <congnhut@dailyxe.com.vn>
2018-10-26 11:05:04 +0700

    khoi tao du an

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace du_an
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
